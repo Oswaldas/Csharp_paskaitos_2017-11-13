dbf946ea1d47318ddbe2b0ee277530f7c06bfd8a
Oswaldas <osvaldas.brazas@gmail.com>
2017-11-14 19:44:42 +0200

    kintamieji

---
+++ 1-1 uzduotis/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_1_uzduotis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var vardas = "osvaldas";
+            var pavarde = "brazas";
+            var amzius = 32;
+            var ugis = 1.83f;
+            var svoris = 75; // kg
+            var aukstoji_mokykla = "ktu";
+            var akademines_grupes_kodas = "sam-6";
+            var kursas = 6;
+            var studiju_programos_pavadinimas = "architektura";
+            var atsiskaitytu_kreditu_skaicius = 12;
+
+        }
+    }
+}
